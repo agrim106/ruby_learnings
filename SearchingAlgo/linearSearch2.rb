cd51871c99ac8a1e4dd5d5a00f600a972ab539c7
Agrim Chaudhary <agrimchaudhary2@gmail.com>
2024-12-20 12:10:23 +0530

    Delete SearchingAlgo directory

--- SearchingAlgo/linearSearch2.rb
+++
@@ -1,9 +0,0 @@
-#Another question on linear search. 
-array = [12,24,36,48,60,72,84,96,108,120]
-def searching(arr , targett)
-  arr.each_with_index  do |element , index|
-    return index  if element == targett
-  end 
-   -1 #If the target is not found
-end 
-puts searching(array , 48) 
