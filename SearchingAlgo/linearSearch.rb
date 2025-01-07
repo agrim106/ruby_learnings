570f470bb4f17c92a9cc8666784f5598fb8b3513
agrim106 <agrimchaudhary2@gmail.com>
2025-01-07 22:14:53 +0530

    [Agrim]MOD:Modded linear search.

--- SearchingAlgo/linearSearch.rb
+++ SearchingAlgo/linearSearch.rb
@@ -1,7 +1,7 @@
 #Linear Search Iterates through each element to find the target value. 
 arr1 = [1,2,3,4,5,66,7,8,9,12]
-def linear_search(arr, target)
-  arr.each_with_index do |element , index| 
+def linear_search(arr1, target)
+  arr1.each_with_index do |element , index| 
     return index if element == target
   end 
     -1 #if the target is not present in the arr then It will return -1.  
