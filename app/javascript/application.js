379f11826f2a965deaa424f9274e083fa17db7c6
agrim106 <agrimchaudhary2@gmail.com>
2025-03-05 14:42:25 +0530

    Fix esbuild errors by removing unnecessary imports

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// import "@hotwired/turbo-rails"
+// import "controllers"
+import Turbolinks from "turbolinks"
+Turbolinks.start()
+console.log("Turbolinks is ready");
