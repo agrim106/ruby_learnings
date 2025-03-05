992a94e6d75eb263fac4f9e5da1f319667a62ab1
agrim106 <agrimchaudhary2@gmail.com>
2025-03-05 15:09:22 +0530

    Add search filter for notes by title or content

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,70 +1,56 @@
 class NotesController < ApplicationController
-  before_action :set_note, only: %i[ show edit update destroy ]
+  before_action :set_note, only: %i[show edit update destroy]
 
-  # GET /notes or /notes.json
   def index
-    @notes = Note.all
+    @notes = params[:keyword].present? ? Note.by_keyword(params[:keyword]) : Note.all
   end
 
-  # GET /notes/1 or /notes/1.json
+  # Rest of the scaffolded actions remain unchanged...
   def show
   end
 
-  # GET /notes/new
   def new
     @note = Note.new
   end
 
-  # GET /notes/1/edit
   def edit
   end
 
-  # POST /notes or /notes.json
   def create
     @note = Note.new(note_params)
-
     respond_to do |format|
       if @note.save
-        format.html { redirect_to @note, notice: "Note was successfully created." }
-        format.json { render :show, status: :created, location: @note }
+        format.html { redirect_to note_url(@note), notice: "Note was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /notes/1 or /notes/1.json
   def update
     respond_to do |format|
       if @note.update(note_params)
-        format.html { redirect_to @note, notice: "Note was successfully updated." }
-        format.json { render :show, status: :ok, location: @note }
+        format.html { redirect_to note_url(@note), notice: "Note was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /notes/1 or /notes/1.json
   def destroy
-    @note.destroy!
-
+    @note.destroy
     respond_to do |format|
-      format.html { redirect_to notes_path, status: :see_other, notice: "Note was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to notes_url, notice: "Note was successfully destroyed." }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_note
-      @note = Note.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def note_params
-      params.expect(note: [ :title, :content ])
-    end
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :content)
+  end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,3 @@
 class Note < ApplicationRecord
+  scope :by_keyword, ->(keyword) { where("title ILIKE ? OR content ILIKE ?", "%#{keyword}%", "%#{keyword}%") }
 end
